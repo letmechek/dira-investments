484c4ee980c5d33966d2a5c8ab2c9640a832d384
letmechek <abdullahi.ali1017@gmail.com>
2023-06-15 12:23:53 -0400

    adds responsive design to invite us

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,14 +3,19 @@ import hero from '../../Assets/images/hero1.webp'
 
 export default function Hero() {
     return (
-      <div className="bg-gray-900 p-4 md:p-8">
-        <div className="bg-black flex flex-col items-center md:flex-row">
-          <img src={hero} alt="hero" className="w-full md:w-1/2 h-auto" />
-          <p className="mt-4 text-white text-4xl md:text-8xl font-bold leading-tight ml-10 p-1" style={{ fontSize: '72px', lineHeight: '1.25em' }}>
-            Become a Valued Partner
-          </p>
+        <div className="bg-gray-900 p-4 md:p-8">
+          <div className="bg-black flex flex-col items-center md:flex-row">
+            <img src={hero} alt="hero" className="w-full md:w-1/2 h-auto" />
+            <div className="ml-10 text-center">
+              <p className="mt-4 text-white text-4xl md:text-8xl font-bold leading-tight p-1" style={{ fontSize: '72px', lineHeight: '1.25em' }}>
+                Become a Valued Partner
+              </p>
+              <p className="text-white font-bold">No Limits</p>
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+      
+      
   }
   
