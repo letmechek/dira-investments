c23accbad2750b084322f5a47747dc6aea524525
letmechek <abdullahi.ali1017@gmail.com>
2023-06-15 16:53:41 -0400

    changes image width

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <div className="bg-gray-900 p-4 md:p-8">
           <div className="bg-black flex flex-col items-center md:flex-row">
-            <img src={hero} alt="hero" className="w-full md:w-1/2 h-auto" style={{height: '650px',width:'60%'}}/>
+            <img src={hero} alt="hero" className="w-full md:w-1/2 h-auto" style={{height: '80vh'}}/>
             <div className="ml-10 text-center">
               <p className="mt-4 text-white text-4xl md:text-8xl font-bold leading-tight p-1" style={{ fontSize: '72px', lineHeight: '1.25em' }}>
                 Become a Valued Partner
